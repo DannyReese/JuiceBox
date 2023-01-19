9cecc48ec1443e29fba0b374624696df21158417
DannyReese <113713130+DannyReese@users.noreply.github.com>
2023-01-18 19:58:57 -0500

    users and api router made

---
+++ api/users.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const usersRouter = express.Router();
+
+
+usersRouter.use((req,res,next)=>{
+    console.log('A request is being made');
+
+    res.send({message:"hello from /users!"});
+
+})
+
+module.exports={
+    usersRouter
+};
--- index.js
+++ index.js
@@ -7,8 +7,17 @@ server.use((req,res,next)=>{
     console.log(req.body);
     console.log('--body Logger END--');
     next()
-})
+});
 
+// server.use('/api', (req, res, next) => {
+//     console.log(req ,"A request was made to /api");
+//     next();
+//   });
+  
+//   server.get('/api', (req, res, next) => {
+//     console.log("A get request was made to /api");
+//     res.send({ message: "success" });
+//   });
 
 server.listen(PORT,()=>{
     console.log(`listening on ${PORT}`);
